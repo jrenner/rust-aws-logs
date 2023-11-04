41b5be87756a15d5bfff558859b6eef9471c81d1
Jon Renner <rennerjc@gmail.com>
2023-11-04 17:10:12 -0500

    refactor

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use clap::Parser;
 use serde::{Deserialize, Serialize};
 use std::process::Command;
 use std::str;
+use std::env;
+use std::path::Path;
 
 use log::{debug, error, info};
 
@@ -43,6 +45,29 @@ struct Event {
     ingestion_time: u64,
 }
 
+
+fn clean_path_str(path: &str) -> String {
+    // replace everything that is not letters or numbers with underscore
+    let mut clean_path = String::new();
+    for c in path.chars() {
+        if c.is_alphanumeric() {
+            clean_path.push(c);
+        } else {
+            clean_path.push('_');
+        }
+    }
+    clean_path
+}
+
+fn verify_aws_cli_tool_available() {
+    let mut cmd = Command::new("aws");
+    cmd.arg("--version");
+    let output = cmd.output().expect("failed to execute process");
+    if !output.status.success() {
+        panic!("aws cli tool not available");
+    }
+}
+
 fn fetch_single_log_page(
     log_group: &str,
     log_stream: &str,
@@ -51,20 +76,20 @@ fn fetch_single_log_page(
     let limit = "10000";
 
     let mut args = vec![
-        "logs".to_string(),
-        "get-log-events".to_string(),
-        "--log-stream-name".to_string(),
-        log_stream.to_string(),
-        "--log-group-name".to_string(),
-        log_group.to_string(),
-        "--start-from-head".to_string(),
-        "--limit".to_string(),
-        limit.to_string(),
+        "logs",
+        "get-log-events",
+        "--log-stream-name",
+        log_stream,
+        "--log-group-name",
+        log_group,
+        "--start-from-head",
+        "--limit",
+        limit,
     ];
 
     if let Some(token) = fwd_token {
-        args.push("--next-token".to_string());
-        args.push(token.to_string());
+        args.push("--next-token");
+        args.push(token);
     }
 
     let mut cmd = Command::new("aws");
@@ -134,8 +159,12 @@ fn fetch_entire_log(log_group: &str, log_stream: &str) -> Vec<Event> {
 fn fetch_entire_log_cached(log_group: &str, log_stream: &str, use_cached: bool) -> Vec<Event> {
     let events: Vec<Event>;
     if use_cached {
-        // try to read cached data from /tmp/aws_log_cache.json
-        let contents = std::fs::read_to_string("/tmp/aws_log_cache.json");
+        let cache_dir = env::temp_dir().join("aws_log_cache");
+        let log_group_safe = clean_path_str(log_group);
+        let log_stream_safe = clean_path_str(log_stream);
+        let full_path = Path::new(&cache_dir).join(log_group_safe).join(log_stream_safe);
+
+        let contents = std::fs::read_to_string(&full_path);
         match contents {
             Ok(contents) => {
                 events = serde_json::from_str(&contents).unwrap();
@@ -146,34 +175,38 @@ fn fetch_entire_log_cached(log_group: &str, log_stream: &str, use_cached: bool) 
                 events = fetch_entire_log(log_group, log_stream);
             }
         }
-    } else {
-        events = fetch_entire_log(log_group, log_stream);
         // now serialize it back into a string
         let serialized = serde_json::to_string(&events).unwrap();
-        // now write to /tmp/aws_log_cache.json
-        std::fs::write("/tmp/aws_log_cache.json", serialized).expect("Unable to write file");
+        // write to cache file
+        // create all directories in full_path if they don't exist
+        std::fs::create_dir_all(&full_path.parent().unwrap()).expect("Unable to create dir for cache file");
+        std::fs::write(&full_path, serialized).expect("Unable to write file");
+        info!("wrote cache file: {}", &full_path.display())
+    } else {
+        events = fetch_entire_log(log_group, log_stream);
     }
     events
 }
 
-fn get_text_from_events(events: &Vec<Event>) -> String {
+fn get_text_from_events(events: &[Event]) -> String {
     let text: String = events
         .iter()
-        .map(|e| e.message.trim().to_string())
-        .collect::<Vec<String>>()
+        .map(|e| e.message.trim())
+        .collect::<Vec<&str>>()
         .join("\n");
     text
 }
 
 fn main() {
     env_logger::init();
+    verify_aws_cli_tool_available();
     let args = Args::parse();
 
     let log_group = args.log_group;
     let log_stream = args.log_stream;
     let output_file = args.output_file;
 
-    let use_cached = false;
+    let use_cached = true;
     let events: Vec<Event> = fetch_entire_log_cached(&log_group, &log_stream, use_cached);
     let full_log_text = get_text_from_events(&events);
 
