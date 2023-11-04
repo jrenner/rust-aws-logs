87a0fd8e738c3744d4ba7ef3b158c422267a4172
Jon Renner <rennerjc@gmail.com>
2023-11-04 12:02:15 -0500

    refactor

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,15 @@ use serde::{Deserialize, Serialize};
 use std::process::Command;
 use std::str;
 
+use log::{debug, error, info};
+
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(short='s', long)]
+    #[arg(short = 's', long)]
     log_stream: String,
 
-    #[arg(short='g', long)]
+    #[arg(short = 'g', long)]
     log_group: String,
 
     #[arg(short, long)]
@@ -21,8 +23,10 @@ struct Args {
 struct EventLog {
     #[serde(rename = "events")]
     events: Vec<Event>,
+
     #[serde(rename = "nextForwardToken")]
     next_forward_token: String,
+
     #[serde(rename = "nextBackwardToken")]
     next_backward_token: String,
 }
@@ -31,13 +35,19 @@ struct EventLog {
 struct Event {
     #[serde(rename = "timestamp")]
     timestamp: u64,
+
     #[serde(rename = "message")]
     message: String,
+
     #[serde(rename = "ingestionTime")]
     ingestion_time: u64,
 }
 
-fn fetch_single_log_page(log_group: &str, log_stream: &str, fwd_token: Option<&str>) -> EventLog {
+fn fetch_single_log_page(
+    log_group: &str,
+    log_stream: &str,
+    fwd_token: Option<&str>,
+) -> Result<EventLog, String> {
     let limit = "10000";
 
     let mut args = vec![
@@ -62,47 +72,54 @@ fn fetch_single_log_page(log_group: &str, log_stream: &str, fwd_token: Option<&s
         cmd.arg(arg);
     }
 
-    //println!("cmd: {:#?}", cmd);
+    debug!("cmd: {:#?}", cmd);
 
     let output = cmd.output().expect("failed to execute process");
-
-    let jdat: EventLog;
+    let event_log: EventLog;
 
     if !output.status.success() {
         let stderr = str::from_utf8(&output.stderr).unwrap();
-        eprintln!("failed to execute process: {}", stderr);
-        panic!("failed to execute process: {}", output.status);
+        let err_msg = "failed to fetch single page of logs:".to_string() + stderr;
+        error!("{}", err_msg);
+        Err(err_msg)
+        //panic!("failed to execute process: {}", output.status);
     } else {
         let stdout = str::from_utf8(&output.stdout).unwrap();
-        jdat = serde_json::from_str(stdout).unwrap();
-        //println!("{:#?}", jdat);
+        event_log = serde_json::from_str(stdout).unwrap();
+        Ok(event_log)
     }
-    jdat
 }
 
 fn fetch_entire_log(log_group: &str, log_stream: &str) -> Vec<Event> {
-
-    // throw error if log_stream begins with "/"
     if log_stream.starts_with("/") {
         panic!("log_stream should probably not begin with / -> {log_stream}");
     }
 
-    println!("fetch entire log - log_group: {log_group}, log_stream: {log_stream}");
+    info!("fetch entire log - log_group: {log_group}, log_stream: {log_stream}");
     let mut i = 0;
     let mut current_token: Option<String> = None;
     let mut all_events = Vec::new();
     loop {
-        let jdat: EventLog =
-            fetch_single_log_page(&log_group, &log_stream, current_token.as_deref());
+        let event_log: EventLog =
+            fetch_single_log_page(&log_group, &log_stream, current_token.as_deref())
+                .unwrap_or_else(|e| panic!("failed to fetch single log page: {}", e));
         // append all the events to all_events
-        all_events.extend(jdat.events);
-        let forward_token: &str = &jdat.next_forward_token;
+        let page_size = event_log.events.len();
+        if page_size == 0 {
+            debug!("page size is 0, break loop");
+            break;
+        }
+        all_events.extend(event_log.events);
+        let forward_token: &str = &event_log.next_forward_token;
         // check if current token is the same as this new forward token
-        let backward_token = jdat.next_backward_token;
-        println!("[{i}] forward_token: {forward_token}, backward_token: {backward_token}");
+        let backward_token = event_log.next_backward_token;
+
+        debug!("[{i}] forward_token: {forward_token}, backward_token: {backward_token}");
+        let n = i + 1;
+        info!("fetched page {n}, size: {page_size}");
+
         if let Some(ref ct) = current_token {
             if ct == &forward_token {
-                println!("current_token == forward_token, breaking");
                 break;
             }
         }
@@ -115,40 +132,41 @@ fn fetch_entire_log(log_group: &str, log_stream: &str) -> Vec<Event> {
 }
 
 fn fetch_entire_log_cached(log_group: &str, log_stream: &str, use_cached: bool) -> Vec<Event> {
-    let jdat: Vec<Event>;
+    let events: Vec<Event>;
     if use_cached {
         // try to read cached data from /tmp/aws_log_cache.json
         let contents = std::fs::read_to_string("/tmp/aws_log_cache.json");
         match contents {
             Ok(contents) => {
-                jdat = serde_json::from_str(&contents).unwrap();
-                println!("using cached data");
+                events = serde_json::from_str(&contents).unwrap();
+                info!("using cached data");
             }
             Err(_) => {
-                println!("no cached data found, running command");
-                jdat = fetch_entire_log(log_group, log_stream);
+                info!("no cached data found, running command");
+                events = fetch_entire_log(log_group, log_stream);
             }
         }
     } else {
-        jdat = fetch_entire_log(log_group, log_stream);
+        events = fetch_entire_log(log_group, log_stream);
         // now serialize it back into a string
-        let serialized = serde_json::to_string(&jdat).unwrap();
+        let serialized = serde_json::to_string(&events).unwrap();
         // now write to /tmp/aws_log_cache.json
         std::fs::write("/tmp/aws_log_cache.json", serialized).expect("Unable to write file");
     }
-    jdat
+    events
 }
 
 fn get_text_from_events(events: &Vec<Event>) -> String {
     let text: String = events
         .iter()
-        .map(|e| e.message.clone())
+        .map(|e| e.message.trim().to_string())
         .collect::<Vec<String>>()
         .join("\n");
     text
 }
 
 fn main() {
+    env_logger::init();
     let args = Args::parse();
 
     let log_group = args.log_group;
@@ -158,11 +176,12 @@ fn main() {
     let use_cached = false;
     let events: Vec<Event> = fetch_entire_log_cached(&log_group, &log_stream, use_cached);
     let full_log_text = get_text_from_events(&events);
-    println!("FULL LOG TEXT:\n{full_log_text}");
 
     if let Some(fpath) = output_file {
         let error_msg = format!("Unable to write file: {fpath}");
-        println!("writing to file: {fpath}");
+        info!("writing to file: {fpath}");
         std::fs::write(&fpath, full_log_text).expect(&error_msg);
+    } else {
+        info!("FULL LOG TEXT:\n{full_log_text}");
     }
 }
