f8c11a5c2823055bb25d774b1486592aa6fe74ca
Jon Renner <jrenner>
2023-11-29 19:13:58 -0600

    use tokio async to fetch all previews at once; all cmds use rust aws sdk

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,10 @@ use std::collections::HashMap;
 use clap::Parser;
 
 use serde::{Deserialize, Serialize};
-use std::env;
-use std::path::Path;
-use std::process::Command;
 use std::str;
 use aws_config::BehaviorVersion;
 
-use rayon::prelude::*;
-
-use log::{debug, error, info};
-use rayon::ThreadPoolBuilder;
+use log::{debug, info};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -36,9 +30,13 @@ struct Args {
     #[arg(short, long)]
     output_file: Option<String>,
 
-    /// get previews of the log streams when listing log groups
-    #[arg(long, action = clap::ArgAction::SetTrue)]
-    preview: bool,
+    /// get previews of the log streams when listing log groups, up to N events
+    #[arg(long, default_value_t = 0)]
+    preview_lines: u32,
+
+    /// get previews of the log streams when listing log groups, up to N most recent streams
+    #[arg(long, default_value_t = 0)]
+    preview_streams: u32,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -56,13 +54,13 @@ struct EventLog {
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct Event {
     #[serde(rename = "timestamp")]
-    timestamp: u64,
+    timestamp: i64,
 
     #[serde(rename = "message")]
     message: String,
 
     #[serde(rename = "ingestionTime")]
-    ingestion_time: u64,
+    ingestion_time: i64,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -92,76 +90,57 @@ struct LogStream {
     creation_time: i64,
 }
 
-fn clean_path_str(path: &str) -> String {
-    // replace everything that is not letters or numbers with underscore
-    let mut clean_path = String::new();
-    for c in path.chars() {
-        if c.is_alphanumeric() {
-            clean_path.push(c);
-        } else {
-            clean_path.push('_');
-        }
-    }
-    clean_path
-}
-
-fn fetch_single_log_page(
+async fn fetch_single_log_page(
+    client: &aws_sdk_cloudwatchlogs::Client,
     log_group: &str,
     log_stream: &str,
     fwd_token: Option<&str>,
     limit: Option<i32>,
 ) -> Result<EventLog, String> {
-    println!("fetch preview for: {log_stream}");
-    let limit_txt = limit.unwrap_or(10000).to_string();
-
-    let mut args = vec![
-        "logs",
-        "get-log-events",
-        "--log-stream-name",
-        log_stream,
-        "--log-group-name",
-        log_group,
-        "--start-from-head",
-        "--limit",
-        &limit_txt,
-    ];
-
+    let token_disp = fwd_token.unwrap_or("None");
+    let limit_disp = limit.unwrap_or(-1);
+    println!("fetch single log page for: {log_stream}, token: {}, limit: {}", token_disp, limit_disp);
+    let mut bld = client.get_log_events()
+        .log_stream_name(log_stream)
+        .log_group_name(log_group)
+        .start_from_head(true);
+    // determine which page to get
     if let Some(token) = fwd_token {
-        args.push("--next-token");
-        args.push(token);
+        bld = bld.next_token(token);
     }
-
-    let mut cmd = Command::new("aws");
-    for arg in args {
-        cmd.arg(arg);
+    if let Some(lmt) = limit {
+        bld = bld.limit(lmt);
     }
-
-    debug!("cmd: {:#?}", cmd);
-
-    let output = cmd.output().expect("failed to execute process");
-    let event_log: EventLog;
-
-    if !output.status.success() {
-        let stderr = str::from_utf8(&output.stderr).unwrap();
-        let err_msg = "failed to fetch single page of logs:".to_string() + stderr;
-        error!("{}", err_msg);
-        Err(err_msg)
-        //panic!("failed to execute process: {}", output.status);
-    } else {
-        let stdout = str::from_utf8(&output.stdout).unwrap();
-        event_log = serde_json::from_str(stdout).unwrap();
-        Ok(event_log)
-    }
+    let response = bld.send().await.unwrap();
+    let events = response.events.unwrap();
+    let my_events = events.into_iter().map(|event| {
+        let timestamp = event.timestamp.unwrap();
+        let message = event.message.unwrap();
+        let ingestion_time = event.ingestion_time.unwrap();
+        Event {
+            timestamp,
+            message,
+            ingestion_time,
+        }
+    }).collect::<Vec<Event>>();
+    let eventlog: EventLog = EventLog {
+        events: my_events,
+        next_forward_token: response.next_forward_token.unwrap(),
+        next_backward_token: response.next_backward_token.unwrap(),
+    };
+    Ok(eventlog)
 }
 
-fn fetch_first_n_events(log_group: &str, log_stream: &str, limit: i32) -> Vec<Event> {
+
+async fn fetch_first_n_events(client: &aws_sdk_cloudwatchlogs::Client, log_group: &str, log_stream: &str, limit: i32) -> Vec<Event> {
     if log_stream.starts_with("/") {
         panic!("log_stream should probably not begin with / -> {log_stream}");
     }
-    info!("fetch first N events from log stream - log_group: {log_group}, log_stream: {log_stream}");
+    info!("fetch first N events from log stream - log_group: {log_group}, log_stream: {log_stream}, limit: {limit}");
     let fwd_token: Option<&str> = None;
     let event_log: EventLog =
-        fetch_single_log_page(&log_group, &log_stream, fwd_token, Some(limit))
+        fetch_single_log_page(client, &log_group, &log_stream, fwd_token, Some(limit))
+            .await
             .unwrap_or_else(|e| panic!("failed to fetch single log page: {}", e));
     // append all the events to all_events
     let page_size = event_log.events.len();
@@ -171,7 +150,7 @@ fn fetch_first_n_events(log_group: &str, log_stream: &str, limit: i32) -> Vec<Ev
     all_events
 }
 
-fn fetch_entire_log(log_group: &str, log_stream: &str) -> Vec<Event> {
+async fn fetch_entire_log(client: &aws_sdk_cloudwatchlogs::Client, log_group: &str, log_stream: &str) -> Vec<Event> {
     if log_stream.starts_with("/") {
         panic!("log_stream should probably not begin with / -> {log_stream}");
     }
@@ -183,7 +162,8 @@ fn fetch_entire_log(log_group: &str, log_stream: &str) -> Vec<Event> {
     loop {
         let limit: Option<i32> = None;
         let event_log: EventLog =
-            fetch_single_log_page(&log_group, &log_stream, current_token.as_deref(), limit)
+            fetch_single_log_page(client, &log_group, &log_stream, current_token.as_deref(), limit)
+                .await
                 .unwrap_or_else(|e| panic!("failed to fetch single log page: {}", e));
         // append all the events to all_events
         let page_size = event_log.events.len();
@@ -211,41 +191,6 @@ fn fetch_entire_log(log_group: &str, log_stream: &str) -> Vec<Event> {
     // sort all the events based on timestamp, just in case they are out of order
     all_events.sort_by(|a, b| a.timestamp.cmp(&b.timestamp));
     all_events
-}
-
-fn fetch_entire_log_cached(log_group: &str, log_stream: &str, use_cache: bool) -> Vec<Event> {
-    let events: Vec<Event>;
-    if use_cache {
-        let cache_dir = env::temp_dir().join("aws_log_cache");
-        let log_group_safe = clean_path_str(log_group);
-        let log_stream_safe = clean_path_str(log_stream);
-        let full_path = Path::new(&cache_dir)
-            .join(log_group_safe)
-            .join(log_stream_safe);
-
-        let contents = std::fs::read_to_string(&full_path);
-        match contents {
-            Ok(contents) => {
-                events = serde_json::from_str(&contents).unwrap();
-                info!("using cached data");
-            }
-            Err(_) => {
-                info!("no cached data found, running command");
-                events = fetch_entire_log(log_group, log_stream);
-            }
-        }
-        // now serialize it back into a string
-        let serialized = serde_json::to_string(&events).unwrap();
-        // write to cache file
-        // create all directories in full_path if they don't exist
-        std::fs::create_dir_all(&full_path.parent().unwrap())
-            .expect("Unable to create dir for cache file");
-        std::fs::write(&full_path, serialized).expect("Unable to write file");
-        info!("wrote cache file: {}", &full_path.display())
-    } else {
-        events = fetch_entire_log(log_group, log_stream);
-    }
-    events
 }
 
 fn get_text_from_events(events: &[Event]) -> String {
@@ -289,45 +234,6 @@ async fn get_sorted_log_stream_names(client: &aws_sdk_cloudwatchlogs::Client, lo
     Ok(log_stream_names)
 }
 
-fn get_sorted_log_stream_names_old(log_group: &str) -> Result<Vec<String>, String> {
-    let mut cmd = Command::new("aws");
-    cmd.args(&[
-        "logs",
-        "describe-log-streams",
-        "--log-group-name",
-        log_group,
-    ]);
-
-    let output = cmd
-        .output()
-        .expect("failed to execute aws logs describe-log-streams");
-    if !output.status.success() {
-        let stderr = str::from_utf8(&output.stderr).unwrap_or("Error: Unable to read stderr");
-        return Err(stderr.to_string());
-    }
-
-    let stdout = str::from_utf8(&output.stdout).unwrap_or("Error: Unable to read stdout");
-    let mut log_streams_response: LogStreamsResponse =
-        serde_json::from_str(stdout).map_err(|e| e.to_string())?;
-
-    log_streams_response.log_streams.sort_by_key(|log_stream| log_stream.creation_time);
-
-
-    let log_stream_names: Vec<String> = log_streams_response
-        .log_streams
-        .into_iter()
-        .map(|stream| stream.log_stream_name)
-        .collect();
-
-    Ok(log_stream_names)
-}
-
-fn init_thread_pool() {
-    // it's not cpu intensive, but we want to do a ton at once
-    let num_threads = 20;
-    ThreadPoolBuilder::new().num_threads(num_threads).build_global().unwrap();
-}
-
 async fn get_cloudwatch_client() -> aws_sdk_cloudwatchlogs::Client {
     let config =
         aws_config::load_defaults(BehaviorVersion::v2023_11_09()).await;
@@ -356,7 +262,6 @@ async fn get_sorted_log_group_names(client: &aws_sdk_cloudwatchlogs::Client) -> 
 async fn main() {
     env_logger::init();
     let args = Args::parse();
-    init_thread_pool();
     let cwl_client = get_cloudwatch_client().await;
     let client = &cwl_client;
 
@@ -379,26 +284,44 @@ async fn main() {
             std::process::exit(1);
         });
         let mut logstream_previews: HashMap<String, String> = HashMap::new();
-        if args.preview {
-            // get the first 10 lines from each log stream of the last few log streams
-            let preview_amount = 6;
+        let preview_requested = args.preview_lines > 0;
+        if preview_requested {
+            // get the first N lines of the last 20 log streams
+            let preview_streams = args.preview_streams;
+            if preview_streams == 0 {
+                println!("--preview-streams must be greater than 0");
+                return;
+            }
+            let preview_event_count = args.preview_lines;
+            let max_preview_events = 200;
+            if preview_event_count > max_preview_events {
+                println!("Preview amount cannot be greater than {max_preview_events}");
+                return;
+            }
             let preview_log_stream_names = log_stream_names
                 .iter()
                 .rev()
-                .take(preview_amount)
+                .take(preview_streams as usize)
                 .map(|s| s.as_str())
                 .collect::<Vec<&str>>();
+            let mut preview_futures = vec![];
+            for log_stream_name in preview_log_stream_names.clone() {
+                let future = fetch_first_n_events(client, &log_group, log_stream_name, preview_event_count as i32);
+                preview_futures.push(future);
+            }
+            let fut_results = futures::future::join_all(preview_futures).await;
 
-            logstream_previews = preview_log_stream_names.par_iter().map(|log_stream| {
-                let n = 10;
-                let events = fetch_first_n_events(&log_group, log_stream.to_string().as_str(), n);
+            for (i, fut_result) in fut_results.into_iter().enumerate() {
+                let log_stream_name = preview_log_stream_names[i];
+                let events = fut_result;
                 let text = get_text_from_events(&events);
-                (log_stream.to_string(), text)
-            }).collect::<HashMap<_, _>>();
+                logstream_previews.insert(log_stream_name.to_string(), text);
+            }
+
         }
         println!("Log Streams (log group: {log_group}):");
         for name in log_stream_names {
-            if args.preview {
+            if preview_requested {
                 println!("\n------------------\n{}", name);
                 // check if it's in the hashmap
                 let is_in_hashmap = logstream_previews.contains_key(&name);
@@ -414,16 +337,7 @@ async fn main() {
     }
 
     let log_stream = args.log_stream.expect("log-stream argument not supplied");
-
-    let use_cache = env::var("ALOG_USE_CACHE")
-        .ok()
-        .and_then(|value| value.parse::<bool>().ok())
-        .unwrap_or(false);
-
-    println!("The value of use_cache is: {}", use_cache);
-
-
-    let events: Vec<Event> = fetch_entire_log_cached(&log_group, &log_stream, use_cache);
+    let events: Vec<Event> = fetch_entire_log(client, &log_group, &log_stream).await;
     let full_log_text = get_text_from_events(&events);
 
     if let Some(fpath) = args.output_file {
